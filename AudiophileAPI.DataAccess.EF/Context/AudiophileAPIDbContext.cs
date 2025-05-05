f495953ea7e36f800aaac893cfb6a78e7e41baff
Leo Loureiro <leolou.90@gmail.com>
2025-05-04 18:00:12 -0700

    Add context

---
+++ AudiophileAPI.DataAccess.EF/Context/AudiophileAPIDbContext.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using AudiophileAPI.DataAccess.EF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AudiophileAPI.DataAccess.EF.Context;
+
+public partial class AudiophileAPIDbContext : DbContext
+{
+    public AudiophileAPIDbContext(DbContextOptions<AudiophileAPIDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<CartItem> CartItems { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8_unicode_ci")
+            .HasCharSet("utf8");
+
+        modelBuilder.Entity<CartItem>(entity =>
+        {
+            entity.HasKey(e => e.CartItemId).HasName("PRIMARY");
+
+            entity.HasIndex(e => e.ProductId, "fk_product_id_idx");
+
+            entity.HasIndex(e => e.UsersId, "fk_user_id_idx");
+
+            entity.Property(e => e.CartItemId).HasColumnType("int(11)");
+            entity.Property(e => e.ProductId).HasColumnType("int(11)");
+            entity.Property(e => e.Quantity).HasColumnType("int(11)");
+            entity.Property(e => e.UsersId).HasColumnType("int(11)");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.CartItems)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("fk_product_idx");
+
+            entity.HasOne(d => d.Users).WithMany(p => p.CartItems)
+                .HasForeignKey(d => d.UsersId)
+                .HasConstraintName("fk_user_idx");
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PRIMARY");
+
+            entity.Property(e => e.CategoryId).HasColumnType("int(11)");
+            entity.Property(e => e.CategoryName).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PRIMARY");
+
+            entity.HasIndex(e => e.UserId, "fk_user_id_idx");
+
+            entity.Property(e => e.OrderId)
+                .ValueGeneratedNever()
+                .HasColumnType("int(11)");
+            entity.Property(e => e.OrderDate).HasColumnType("datetime");
+            entity.Property(e => e.ShippingAddress).HasColumnType("text");
+            entity.Property(e => e.Status).HasMaxLength(20);
+            entity.Property(e => e.TotalAmount).HasPrecision(10, 2);
+            entity.Property(e => e.UserId).HasColumnType("int(11)");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("fk_user_id");
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.HasKey(e => e.OrderDetailId).HasName("PRIMARY");
+
+            entity.HasIndex(e => e.OrderId, "fk_order_id_idx");
+
+            entity.HasIndex(e => e.ProductId, "fk_product_id_idx");
+
+            entity.Property(e => e.OrderDetailId).HasColumnType("int(11)");
+            entity.Property(e => e.OrderId).HasColumnType("int(11)");
+            entity.Property(e => e.Price).HasPrecision(10, 2);
+            entity.Property(e => e.ProductId).HasColumnType("int(11)");
+            entity.Property(e => e.Quantity).HasColumnType("int(11)");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.OrderId)
+                .HasConstraintName("fk_order_id");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("fk_product_id");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PRIMARY");
+
+            entity.HasIndex(e => e.CategoryId, "fk_category_id_idx");
+
+            entity.Property(e => e.ProductId).HasColumnType("int(11)");
+            entity.Property(e => e.CategoryId).HasColumnType("int(11)");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.Features).HasColumnType("text");
+            entity.Property(e => e.ImageUrl).HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Price).HasPrecision(10, 2);
+            entity.Property(e => e.Stock).HasColumnType("int(11)");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("fk_category_id");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UsersId).HasName("PRIMARY");
+
+            entity.Property(e => e.UsersId).HasColumnType("int(11)");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.PasswordHash).HasMaxLength(255);
+            entity.Property(e => e.Role).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
