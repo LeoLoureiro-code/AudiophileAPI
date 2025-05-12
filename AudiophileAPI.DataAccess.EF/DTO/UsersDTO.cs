33a13546b007f7f1ff9624f01d31fa90251d4f2b
Leo Loureiro <leolou.90@gmail.com>
2025-05-11 18:58:04 -0700

    Add required field to some attributes

--- AudiophileAPI.DataAccess.EF/DTO/UsersDTO.cs
+++ AudiophileAPI.DataAccess.EF/DTO/UsersDTO.cs
@@ -1,11 +1,22 @@
-﻿namespace AudiophileAPI.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AudiophileAPI.DTO
 {
     public class UsersDTO
     {
+        [Required]
         public string FirstName { get; set; } = null!;
+
+        [Required]
         public string LastName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = null!;
+
+        [Required]
         public string Password { get; set; } = null!;
+
         public string Role { get; set; } = null!;
     }
 }
