8eade0f9d41fedf093d347457c02662e399a76ac
Leo Loureiro <leolou.90@gmail.com>
2025-05-11 18:57:36 -0700

    Add dependency injection to all interfaces

--- AudiophileAPI/Program.cs
+++ AudiophileAPI/Program.cs
@@ -1,4 +1,7 @@
 using AudiophileAPI.DataAccess.EF.Context;
+using AudiophileAPI.DataAccess.EF.Interfaces;
+using AudiophileAPI.DataAccess.EF.Repositories;
+using AudiophileAPI.DataAccess.EF.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,12 +12,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<IPasswordService, PasswordService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddDbContext<AudiophileAPIDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
     ));
+
 
 var app = builder.Build();
 
